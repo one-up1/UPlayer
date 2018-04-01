d95b4b0d71a41f14d872a99e82087827be5a7485
Ferenc Dykstra <oneup1@gmail.com>
2018-04-01 13:42:24 +0200

    - Code cleanup.

--- app/src/main/java/com/oneup/uplayer/MainReceiver.java
+++ app/src/main/java/com/oneup/uplayer/MainReceiver.java
@@ -3,6 +3,7 @@ package com.oneup.uplayer;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.util.Log;
 
 import com.oneup.uplayer.activity.PlaylistActivity;
@@ -10,29 +11,38 @@ import com.oneup.uplayer.activity.PlaylistActivity;
 public class MainReceiver extends BroadcastReceiver {
     private static final String TAG = "UPlayer";
 
+    private static final String ARG_STATE = "state";
+    private static final String KEY_HEADSET_STATE = "headsetState";
+
+    private Context context;
+    private SharedPreferences preferences;
+
     @Override
     public void onReceive(Context context, Intent intent) {
+        this.context = context;
+        preferences = context.getSharedPreferences(TAG, 0);
+
         String action = intent.getAction();
         Log.d(TAG, "MainReceiver.onReceive(" + action + ")");
 
-        if (action.equals(Intent.ACTION_HEADSET_PLUG)) {
-            onHeadsetPlug(context, intent.getIntExtra("state", -1));
+        if (action != null && action.equals(Intent.ACTION_HEADSET_PLUG)) {
+            onHeadsetPlug(intent.getIntExtra(ARG_STATE, -1));
         }
     }
 
-    private void onHeadsetPlug(Context context, int state) {
+    private void onHeadsetPlug(int state) {
         Log.d(TAG, "MainReceiver.onHeadsetPlug(" + state + ")");
 
         switch (state) {
             case 0:
                 Log.d(TAG, "Headset unplugged");
-                if (context.getSharedPreferences(TAG, 0).getInt("headset_state", 0) == 1) {
+                if (preferences.getInt(KEY_HEADSET_STATE, 0) == 1) {
                     PlaylistActivity.finishIfRunning();
                     context.stopService(new Intent(context, MainService.class));
                 }
                 break;
         }
 
-        context.getSharedPreferences(TAG, 0).edit().putInt("headset_state", state).apply();
+        preferences.edit().putInt(KEY_HEADSET_STATE, state).apply();
     }
 }

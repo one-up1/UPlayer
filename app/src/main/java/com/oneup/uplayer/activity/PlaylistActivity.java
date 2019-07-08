73ea672b5aeb66e856e2ec6038175102ee06365c
oneup <oneup1@gmail.com>
2019-07-08 11:07:25 +0200

    - Removed scrolling to current song in PlaylistActivity.onResume().

--- app/src/main/java/com/oneup/uplayer/activity/PlaylistActivity.java
+++ app/src/main/java/com/oneup/uplayer/activity/PlaylistActivity.java
@@ -80,16 +80,6 @@ public class PlaylistActivity extends AppCompatActivity {
         }
 
         @Override
-        public void onResume() {
-            Log.d(TAG, "PlaylistFragment.onResume()");
-            super.onResume();
-
-            if (service != null) {
-                setSelection();
-            }
-        }
-
-        @Override
         public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
             inflater.inflate(R.menu.fragment_playlist, menu);
             super.onCreateOptionsMenu(menu, inflater);
@@ -253,10 +243,6 @@ public class PlaylistActivity extends AppCompatActivity {
             service.update();
         }
 
-        private void setSelection() {
-            setSelection(service.getPlaylist().getSongIndex());
-        }
-
         private static PlaylistFragment newInstance() {
             return new PlaylistFragment();
         }
@@ -277,7 +263,7 @@ public class PlaylistActivity extends AppCompatActivity {
                 service.setOnUpdateListener(PlaylistFragment.this);
 
                 reloadData();
-                setSelection();
+                setSelection(service.getPlaylist().getSongIndex());
             }
 
             @Override
